119fa8836117dbba0694cefc5f7ee9d582e40e28
Lissa Walzer <lissawalzer@gmail.com>
2016-01-17 12:27:12 -0800

    style changes

--- controllers/router.js
+++ controllers/router.js
@@ -4,11 +4,15 @@ var bodyParser = require('body-parser');
 //var knex = require('../db/knex');
 
 router.get('/', function(req, res) {
-    res.render('index')
+    res.render('login')
 })
 
-router.get('/login', function(req, res) {
-    res.render('login')
+router.post('/signin', function(req, res) {
+    res.redirect('/index')
+})
+
+router.get('/index', function(req, res) {
+    res.render('index')
 })
 
 module.exports = router;
