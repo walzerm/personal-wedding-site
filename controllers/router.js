8b29c22d0fbeccd94f38f4d81bd0681f3aedf549
Lissa Walzer <lissawalzer@gmail.com>
2016-01-17 18:47:30 -0800

    Testing knex insertion

--- controllers/router.js
+++ controllers/router.js
@@ -1,14 +1,17 @@
 var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
-//var knex = require('../db/knex');
+var knex = require('../db/knex');
 
 router.get('/', function(req, res) {
     res.render('login')
 })
 
 router.post('/signin', function(req, res) {
-    res.redirect('/index')
+    knex('rsvp').insert({guest_name: req.body.inputPassword}).then(function() {
+        res.redirect('/index')
+    })
+
 })
 
 router.get('/index', function(req, res) {
