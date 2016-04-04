95c5825f1eec7a83c7f05e2e07d97638df8b94d6
Lissa Walzer <lissawalzer@gmail.com>
2016-04-04 14:59:19 -0700

    Auth works now, but doesn't hash password

--- controllers/main.js
+++ controllers/main.js
@@ -4,13 +4,14 @@ var bodyParser = require('body-parser');
 var knex = require('../db/knex');
 
 
-router.get('/index', isLoggedIn, function(req, res, next) {
+router.get('/index', isLoggedIn, function(req, res) {
+    console.log('i got to index');
     res.render('index')
 })
 
 // route middleware to make sure a user is logged in
 function isLoggedIn(req, res, next) {
-
+    console.log(req.isAuthenticated());
     // if user is authenticated in the session, carry on
     if (req.isAuthenticated())
         return next();
--- server.js
+++ server.js
@@ -5,55 +5,78 @@ var express = require('express'),
     passport = require('passport'),
     LocalStrategy = require('passport-local').Strategy,
     bcrypt = require('bcrypt'),
-    cookieParser = require('cookie-parser')
+    cookieParser = require('cookie-parser'),
+    knex = require('./db/knex'),
+    morgan = require('morgan'),
+    flash = require('connect-flash'),
     expressSession = require('express-session');
-
 
 //Middleware
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
+app.use(morgan('dev'));
+
+app.use(expressSession({
+  secret: 'supersecret',
+  resave: true,
+  saveUninitialized: true
+}));
+
+//Passport for authentication
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(flash());
 
 
 //Set views
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-//Authenticate
-// passport.serializeUser(function(userID, done) {
-//     done(null, userID);
-// })
-//
-// passport.deserializeUser(function(userID, done) {
-//     knex('party_numbers').where('userID', userID).first().then(function(user) {
-//         done(null, user);
-//     })
-// })
-//
-// passport.use('login', new LocalStrategy({
-//     userID: 'userID',
-//     password: 'password',
-//     passReqToCallback: true
-//     },
-//     function(req, userID, password, done) {
-//         knex('party_numbers').where('userID', userID).first().then(function(user) {
-//             if (!user || !bcrypt.compareSync(password, user.password)) {
-//                 return done(null, false, req.flash('message', 'Invalid userID or password'));
-//             }
-//             return done(null, user.userID);
-//         })
-//     }
-// ))
-//
-// app.use(function (req, res, next) {
-//     res.locals.loggedIn = req.isAuthenticated();
-// })
-
 //Set router
-var router = require('./controllers/router');
-app.use(router);
+var authRoutes = require('./controllers/auth');
+var mainRoutes = require('./controllers/main');
+
+
+//Authenticate
+passport.serializeUser(function(userID, done) {
+    done(null, userID);
+})
+
+passport.deserializeUser(function(userID, done) {
+    knex('party_numbers').where('user_id', userID).first().then(function(user) {
+        done(null, user);
+    })
+})
+
+passport.use('login', new LocalStrategy({
+    usernameField: 'userID',
+    passwordField: 'password',
+    passReqToCallback: true
+    },
+    function(req, userID, password, done) {
+        knex('party_numbers').where('user_id', parseInt(userID)).first().then(function(user) {
+            console.log('here!');
+
+            //use this once password is encrypted !bcrypt.compareSync(password, user.password)
+            if (!user || password !== user.password) {
+                return done(null, false);
+            }
+            return done(null, user.user_id);
+        })
+    }
+))
+
+//Middleware to check if user is logged in
+app.use(function (req, res, next) {
+    res.locals.login = req.isAuthenticated();
+    next();
+})
+
+//Use routes
+app.use(authRoutes);
+app.use(mainRoutes);
+
 
 app.listen(process.env.PORT || 8000, function() {
     console.log('Server going at 8000...');
