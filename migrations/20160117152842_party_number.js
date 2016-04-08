6f5eb709f86a4958d6d3d91d9ddab7483b8c8682
Lissa Walzer <lissawalzer@gmail.com>
2016-04-08 11:10:13 -0700

    Updated party_number table

--- migrations/20160117152842_party_number.js
+++ migrations/20160117152842_party_number.js
@@ -2,7 +2,7 @@
 exports.up = function(knex, Promise) {
     return knex.schema.createTable('party_numbers', function(table) {
         table.increments();
-        table.integer('user_id');
+        table.string('group_name');
         table.string('password');
         table.integer('atendees');
     });
