ce5a8a513a1a6a9587ca88439b20bcc839569381
Lissa Walzer <lissawalzer@gmail.com>
2016-04-18 15:14:49 -0700

    Added smooth scroll to rsvp button

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,18 @@
 $(document).ready(function() {
 
 
-    $(".custom-close").on('click', function() {
-        $('#alreadyModal').modal('hide');
-    });
+    $('.smoothScroll').click(function() {
+   if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+     var target = $(this.hash);
+     target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+     if (target.length) {
+       $('html,body').animate({
+         scrollTop: target.offset().top
+       }, 1000); // The number here represents the speed of the scroll in milliseconds
+       return false;
+     }
+   }
+ });
 
 
 })
