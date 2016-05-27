8c81e39da37efa4a538ac03637c0ea02ebada50c
Roger Tan <roger.tan@airbnb.com>
2016-05-26 21:40:07 -0700

    lower case user names on login

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ passport.use('login', new LocalStrategy({
     },
     function(req, userID, password, done) {
         //Query the table for user
-        knex('party_numbers').where('group_name', userID).first().then(function(user) {
+        knex('party_numbers').where('group_name', userID.toLowerCase()).first().then(function(user) {
             if (!user) {
                 // couldn't find user
                 return done(null, false, req.flash('loginMessage', 'Incorrect username and/or password'));
